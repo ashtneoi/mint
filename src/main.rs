df4816fc7e64829360523943caa22cfc05e0aca1
ashtneoi <ashtneoi@gmail.com>
2018-03-26 12:44:32 -0700

    Pull environ builder out into function

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,29 @@ fn print_usage() {
 }
 
 
+fn args_to_environ(args_vec: &Vec<String>) -> HashMap<&str, &str> {
+    let mut args = args_vec.iter();
+
+    let mut environ = HashMap::<&str, &str>::new();
+    loop {
+        let pair = match args.next() {
+            None => break,
+            Some(p) => p,
+        };
+
+        let splat: Vec<&str> = pair.splitn(2, '=').collect();
+        if splat.len() != 2 {
+            print_usage();
+            exit(2);
+        }
+        let (name, val) = (splat[0], splat[1]);
+        environ.insert(name, val);
+    }
+
+    environ
+}
+
+
 fn main() {
     let mut args = env::args();
 
@@ -22,24 +45,8 @@ fn main() {
     };
 
     let environ_args_vec: Vec<String> = args.collect();
-    let mut environ_args = environ_args_vec.iter();
-
-    let mut environ = HashMap::<&str, &str>::new();
+    let mut environ = args_to_environ(&environ_args_vec);
     environ.insert("#top_name", &tmpl_name);
-    loop {
-        let pair = match environ_args.next() {
-            None => break,
-            Some(p) => p,
-        };
-
-        let splat: Vec<&str> = pair.splitn(2, '=').collect();
-        if splat.len() != 2 {
-            print_usage();
-            exit(2);
-        }
-        let (name, val) = (splat[0], splat[1]);
-        environ.insert(name, val);
-    }
 
     for (name, val) in &environ {
         println!("{} = \"{}\"", name, val);
