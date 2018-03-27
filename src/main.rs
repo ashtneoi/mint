608961a01721e6b41546e858b34a72925f49010f
ashtneoi <ashtneoi@gmail.com>
2018-03-27 01:38:13 -0700

    Keep track of tags

--- src/main.rs
+++ src/main.rs
@@ -59,7 +59,32 @@ fn main() {
         )
     ).collect();
 
-    for line in &lines {
-        println!("{}", line);
-    };
+    let mut tags: Vec<(usize, usize, usize)> = Vec::new();
+
+    let open_pat = "{{";
+    let close_pat = "}}";
+    for (row, line) in lines.iter().enumerate() {
+        let mut first = true;
+        let mut chunk_start: usize = 0;
+        for chunk in line.split(open_pat) {
+            if first {
+                first = false;
+            } else {
+                if !chunk.starts_with('!') {
+                    let tag_end_rel = chunk.find(close_pat).unwrap_or_else(|| {
+                        eprintln!("{}: Missing \"{}\"", row+1, close_pat);
+                        exit(1);
+                    });
+                    let tag_start = chunk_start - open_pat.len();
+                    let tag_end = chunk_start + tag_end_rel + close_pat.len();
+                    tags.push((row, tag_start, tag_end));
+                }
+            }
+            chunk_start += chunk.len() + open_pat.len();
+        }
+    }
+
+    for (row, col_from, col_to) in tags {
+        println!("{} {}", row, &lines[row][col_from .. col_to]);
+    }
 }
