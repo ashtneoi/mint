e75ee849df1bd3066be0b18371fdca027d17ad5c
ashtneoi <ashtneoi@gmail.com>
2018-04-23 01:11:43 -0700

    Print usage message to stderr

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use lib::{do_file, Mint, SliceAsStrs};
 mod tests;
 
 fn exit_with_usage() -> ! {
-    println!("Usage: mint TMPLPATH [NAME=VAL ...]");
+    eprintln!("Usage: mint TMPLPATH [NAME=VAL ...]");
     exit(2);
 }
 
